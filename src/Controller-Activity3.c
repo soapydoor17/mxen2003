c1294896be9d046c1c0af0c4c30d3a753018e56c
soapydoor17 <161790896+soapydoor17@users.noreply.github.com>
2024-03-04 13:44:38 +0800

    Update and rename Controller.c to Controller-Activity3.c

--- src/Controller-Activity3.c
+++ src/Controller-Activity3.c
@@ -7,17 +7,22 @@
 
 //static function prototypes, functions only called in this file
 
+// ACTIVITY 3: Demonstrate two LEDs blinking alternately
+
 int main(void)
 {
-  DDRA = 0xFF;//put PORTA into output mode
+  DDRA = 0xFF; //put PORTA into output mode
   PORTA = 0; 
-  while(1)//main loop
+  while(1) //main loop
   {
     _delay_ms(500);     //500 millisecond delay
     PORTA |= (1<<PA3);  // note here PA3 is just an alias for the number 3
                         // this line is equivalent to PORTA = PORTA | 0b00001000   which writes a HIGH to pin 3 of PORTA
+    PORTA &= ~(1<<PA5); // sets pin five to low
+
     _delay_ms(500); 
+    PORTA |= (1<<PA5);  // sets pin five to high
     PORTA &= ~(1<<PA3); // this line is equivalent to PORTA = PORTA & (0b11110111)  which writes a HIGH to pin 3 of PORTA
   }
   return(1);
-}//end main 
+} //end main
