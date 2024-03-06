4537c8737529d81cdd5cf2bd1b55258bc63d1ab7
finn-purcy <162277880+finn-purcy@users.noreply.github.com>
2024-03-06 15:42:48 +0800

    Update Thermobitch.c
    try 1

--- src/Thermobitch.c
+++ src/Thermobitch.c
@@ -5,7 +5,6 @@
 //include this .c file's header file
 #include "Controller.h"
 #include "../lib/adc/adc.h" // minimal adc lib
-
 //static function prototypes, functions only called in this file
 
 int main(void)
@@ -16,18 +15,16 @@ int main(void)
   DDRC = 0xFF;       // put PORTC into output mode
   DDRF = 0;          // put PORTF into input mode
   PORTC = 0;         // All digital outputs of C are LOW
-  //PORTF |= (1<<PF7);  // pull up resistor for button
   uint16_t reading = 0;  // where joystick reading is assigned
-  uint16_t MinVoltage = <Vmin>*1024/5; //convert voltages to 10bit 
-  uint16_t MaxVoltage = <Vmax>*1024/5; //convert voltages to 10bit
-  uint8_t ledOutput = 0;
+  uint16_t MinVoltage = 492; //convert voltages to 10bit 
+  uint16_t MaxVoltage = 450; //convert voltages to 10bit
+  uint16_t ledOutput = 0;
     
   while(1) //main loop
   {
     reading = adc_read(5);
-    ledOutput = (reading - MinVoltage) / (MaxVoltage - MinVoltage) * 256; // convert reading to 8bit
-    // reading = reading >> 2; // most significant bits shift down so its 8 bit
-    PORTC = ledOutput;        // display 8 bit reading on LEDs
+    ledOutput = (MinVoltage - reading) / (MinVoltage - MaxVoltage) * 255; // convert reading to 8bit
+    PORTC = (uint8_t) ~ledOutput;        // display 8 bit reading on LEDs
     
   }
   return(1);
