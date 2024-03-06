1167d5d1c2e12f78124ff1d31eb1b9a89e2e68f1
finn-purcy <162277880+finn-purcy@users.noreply.github.com>
2024-03-06 12:59:59 +0800

    Update Thermobitch.c

--- src/Thermobitch.c
+++ src/Thermobitch.c
@@ -16,14 +16,19 @@ int main(void)
   DDRC = 0xFF;       // put PORTC into output mode
   DDRF = 0;          // put PORTF into input mode
   PORTC = 0;         // All digital outputs of C are LOW
-  PORTF |= (1<<PF7);  // pull up resistor for button
+  //PORTF |= (1<<PF7);  // pull up resistor for button
   uint16_t reading = 0;  // where joystick reading is assigned
-
+  uint16_t MinVoltage = <Vmin>*1024/5; //convert voltages to 10bit 
+  uint16_t MaxVoltage = <Vmax>*1024/5; //convert voltages to 10bit
+  uint8_t ledOutput = 0;
+    
   while(1) //main loop
   {
     reading = adc_read(5);
-    reading = reading >> 2; // most significant bits shift down so its 8 bit
-    PORTC = reading;        // display 8 bit reading on LEDs
+    ledOutput = (reading - MinVoltage) / (MaxVoltage - MinVoltage) * 256; // convert reading to 8bit
+    // reading = reading >> 2; // most significant bits shift down so its 8 bit
+    PORTC = ledOutput;        // display 8 bit reading on LEDs
+    
   }
   return(1);
 } //end main 
