56b550df3abd22e484ba26a7b0cee4cac5965ea0
soapydoor17 <scdonly@gmail.com>
2024-03-08 17:34:55 +0800

    Week 3 Pre Lab Video

--- src/Controller.c
+++ src/Controller.c
@@ -10,28 +10,22 @@
 
 int main(void)
 {
-  adc_init(); // initialises ADC
-  _delay_ms(20);
-
-  DDRC = 0xFF;       // put PORTC into output mode
-  DDRF = 0;          // put PORTF into input mode
-  PORTC = 0;         // All digital outputs of C are LOW
-  PORTF |= (1<<PF7);  // pull up resistor for button
-  uint16_t reading = 0;  // where joystick reading is assigned
+  // interrupt setup
+  cli();             // Clear interrupts
+  DDRD &= ~(1<<PD0); // INT0 input mode
+  EICRA |= (1<<ISC01);
+  EICRA &= ~(1<<ISC00); // INT0 set falling edge trigger
+  EIMSK |= (1<<INT0);   // INT0 enable
+  sei();
 
   while(1) //main loop
   {
-    if (!(PINF & (1<<PF7))) // if button is pressed read PF5 else read PF3
-    {
-      reading = adc_read(5);
-    }
-    else{
-      reading = adc_read(3);
-    }
-
-    reading = reading >> 2; // most significant bits shift down so its 8 bit
-    PORTC = reading;        // display 8 bit reading on LEDs
-
+    
   }
   return(1);
 } //end main 
+
+ISR(INT0_vect)
+{
+  // interrupt code here
+}
