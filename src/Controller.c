73a6a3b9c91d801fc225cb257b4f49d1bb893bd7
finn-purcy <162277880+finn-purcy@users.noreply.github.com>
2024-03-20 21:06:42 +0800

    code skeleton

--- src/Controller.c
+++ src/Controller.c
@@ -1,4 +1,4 @@
-//Example ATmega2560 Project
+//week5 lab work
 //File: ATmega2560Project.c
 //An example file for second year mechatronics project
 
@@ -7,31 +7,51 @@
 #include "../lib/adc/adc.h" // minimal adc lib
 
 //static function prototypes, functions only called in this file
+bool timer_start = 1;
 
 int main(void)
 {
-  adc_init(); // initialises ADC
-  _delay_ms(20);
 
-  DDRC = 0xFF;       // put PORTC into output mode
-  DDRF = 0;          // put PORTF into input mode
-  PORTC = 0;         // All digital outputs of C are LOW
-  PORTF |= (1<<PF7);  // pull up resistor for button
-  uint16_t reading = 0;  // where joystick reading is assigned
+  adc_init(); // initialse ADC
+	lcd_init(); // initialise LCD
+	_delay_ms(20);
 
+  DDRD = 0;			// PortD input for button
+  PORTD = 0;  // PinD0 for button
+	PORTD |=(1<<PD0);	// pull-up resistors for button
+  
+  cli();
+  TTCR1A = 0;
+  TTCR1B |= (1<<WGM12)|(1<<WGM13)|(1<<CS10);  //timer set for mode 12 (CTC) AND prescaler of 1
+  TCNT1 = 0;
+  ICR1 = 1000;  //sets top value NEEDS TO BE CALCULATED
+  TIMSK1 = (1<<ICIE1);  //capture interupt for timer 1
+  sei();
+  
   while(1) //main loop
   {
-    if (!(PINF & (1<<PF7))) // if button is pressed read PF5 else read PF3
-    {
-      reading = adc_read(5);
-    }
-    else{
-      reading = adc_read(3);
-    }
-
-    reading = reading >> 2; // most significant bits shift down so its 8 bit
-    PORTC = reading;        // display 8 bit reading on LEDs
+    if timer_start == 1;
+      LCD
+   
 
   }
   return(1);
 } //end main 
+
+ISR(TIMER1_CAPT_vect)
+{
+  //interrupt increment
+}
+
+
+//stop start toggle
+ISR(INT0_vect)
+{
+	uint32_t currTime = milliseconds_now();
+	static uint32_t prevTime = 0;
+	if((currTime-prevTime) < DEBOUNCE_PERIOD)
+	{
+		timer_start ^= 1;
+    prevTime = currTime;
+	}
+}
