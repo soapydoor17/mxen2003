6a0042fa895c443a01768e9e49b9e18e58cca76f
soapydoor17 <161790896+soapydoor17@users.noreply.github.com>
2024-03-27 15:47:54 +0800

    Update Controller.c

--- src/Controller.c
+++ src/Controller.c
@@ -9,85 +9,45 @@
 //static function prototypes, functions only called in this file
 
 // Global Variables
-volatile uint8_t t_millis = 0;
-volatile uint8_t t_secs = 0;
-volatile uint16_t t_mins = 0;
 
 int main(void)
 {
   cli();
 	
-  // Variable Initialising
-  char line1_string[33] ={0};
 
   // Port Initialising
-  DDRD = 0;                        // put PORTD into input mode
-  PORTD |= (1<<PD0) | (1<<PD1);    // enable pull-up for PD0 and PD1
+  DDRC = 0;                        // put PORTC into input mode
+  DDRB |= (1<<PB5);
 
-  DDRB = 0xFF;
-  	uint16_t compValue = 0;
-	0CR1A = compValue;
-
-  // Button Interrupts
-  EICRA |= (1<<ISC01);
-  EICRA &= ~(1<<ISC00);            // INT0 to falling edge trigger
-  EIMSK |= (1<<INT0);              // enable INT0
-  EICRA |= (1<<ISC11);
-  EICRA &= ~(1<<ISC10);            // INT1 to falling edge trigger
-  EIMSK |= (1<<INT1);              // enable INT1
 
   // Timer Interrups
-  TCCR1A = 0;
+  TCCR1A = (1<<COM1A1);
   TCCR1B |= (1<<WGM13) | (1<<CS11);
   TCNT1 = 0;
   ICR1 = 20000;
+
+  uint16_t compVal = 10000; // need to calc
+  OCR1A = compVal;
+  
   TIMSK1 = (1<<ICIE1);
 
   // library initialising
-  
-  lcd_init();
+  adc_init();
+  //lcd_init();
   _delay_ms(20);
 
   sei();
 
   while(1) //main loop
   {
-    sprintf(line1_string, "%02u : %02u . %02u", t_mins, t_secs, t_millis);
-    lcd_home();
-    lcd_puts(line1_string);
   }
   return(1);
 } //end main 
 
-// BUTTON 1: Start/Stop
-ISR(INT0_vect)
-{
-  uint32_t currTime = milliseconds_now();
-	static uint32_t prevTime = 0;
-	if((currTime-prevTime) < DEBOUNCE_PERIOD)
-	{
-		TIMSK1 ^= (1<<OCIE1A);
-    prevTime = currTime;
-	}
-}
 
-// BUTTON 2: Reset
-ISR(INT1_vect)
-{
-  uint32_t currTime = milliseconds_now();
-	static uint32_t prevTime = 0;
-	if((currTime-prevTime) < DEBOUNCE_PERIOD)
-	{
-	TCNT1 = 0;
-   	t_millis = 0;
-	t_secs = 0;
-        t_mins = 0;
-	prevTime = currTime;
-	}
-}
 
 // Timer Interrupt
 ISR(TIMER1_CAPT_vect)
 {
-    }
+
 }
