967a38fe511ee65abff8d3c7c4cd3f021a2ff3ce
soapydoor17 <scdonly@gmail.com>
2024-04-24 10:02:05 +0800

    week 8 fun stuff

--- src/Controller.c
+++ src/Controller.c
@@ -10,27 +10,51 @@
 
 int main(void)
 {
-  adc_init(); // initialises ADC
+  serial0_init(); 	// terminal communication with PC
+  serial2_init();		// microcontroller communication to/from another Arduino
+  milliseconds_init();
+  adc_init();
   _delay_ms(20);
 
-  DDRC = 0xFF;       // put PORTC into output mode
-  DDRF = 0;          // put PORTF into input mode
-  PORTC = 0;         // All digital outputs of C are LOW
-  PORTF |= (1<<PF7);  // pull up resistor for button
-  uint16_t reading = 0;  // where joystick reading is assigned
+  uint8_t sendDataByte1=0, sendDataByte2=0;		// data bytes sent
+  uint32_t current_ms=0, last_send_ms=0;			// used for timing the serial send
+
+  uint16_t x_reading=0, y_reading=0;  // reading of joysticks to be sent to robot
+
+  DDRF = 0;          // put PORTF into input mode for joysticks
+
+  UCSR2B |= (1 << RXCIE2); // Enable the USART Receive Complete interrupt (USART_RXC)
+	
+  sei();
 
   while(1) //main loop
   {
-    if (!(PINF & (1<<PF7))) // if button is pressed read PF5 else read PF3
+    // Sending information
+    current_ms = milliseconds_now();
+    if(current_ms-last_send_ms >= 100)
     {
-      reading = adc_read(5);
-    }
-    else{
-      reading = adc_read(3);
-    }
+      // Read joysticks and convert for sending
+      y_reading = adc_read(0);
+      x_reading = adc_read(1);
 
-    reading = reading >> 2; // most significant bits shift down so its 8 bit
-    PORTC = reading;        // display 8 bit reading on LEDs
+      sendDataByte1 = y_reading / 4;
+      if (sendDataByte1 > 253)
+      {
+        sendDataByte1 = 253;
+      }
+
+      sendDataByte2 = x_reading / 4;
+      if (sendDataByte2 > 253)
+      {
+        sendDataByte2 = 253;
+      }
+
+      last_send_ms = current_ms;
+      serial2_write_byte(0xFF);
+      serial2_write_byte(sendDataByte1);
+      serial2_write_byte(sendDataByte2);
+      serial2_write_byte(0xFE);
+    }
 
   }
   return(1);
--- src/Robot.c
+++ src/Robot.c
@@ -6,18 +6,114 @@
 #include "Robot.h"
 
 //static function prototypes, functions only called in this file
+volatile uint8_t dataByte1=0, dataByte2=0;
+volatile bool new_message_received_flag=false;
 
 int main(void)
 {
-  DDRA = 0xFF;//put PORTA into output mode
-  PORTA = 0; 
+  // initialisation
+	serial0_init(); 	// terminal communication with PC
+	serial2_init();		// microcontroller communication to/from another Arduino
+  milliseconds_init();
+  _delay_ms(20);
+	
+  // variables
+  uint16_t fc=0, rc=0;              // Joystick readings received from controller
+  static int16_t lm=0, rm =0;       // Speed and direction of motors
+
+  DDRA |= (1<<DDA0)|(1<<DDA1)|(1<<DDA2)|(1<<DDA3);      // put PORTA into output mode for motors
+  PORTA = 0;
+
+  DDRE |= (1<<PE3)|(1<<PE4);        // PORTE output mode for motors
+  OCR3A = 8000;
+  OCR3B = 8000;
+  TCCR3A |= (1<<COM3A1) | (1<<COM3B1);    // OC3A and OC3B clears on compare match
+  TCCR3B |= (1<<WGM33) | (1<<CS31);       // WGM 8 and Prescaler of 8
+  ICR3 = 10000;                           // TOP is 16000
+
+	UCSR2B |= (1 << RXCIE2); // Enable the USART Receive Complete interrupt (USART_RXC)
+
+  sei();
+
   while(1)//main loop
   {
-    _delay_ms(500);     //500 millisecond delay
-    PORTA |= (1<<PA2);  // note here PA3 is just an alias for the number 3
-                        // this line is equivalent to PORTA = PORTA | 0b00001000   which writes a HIGH to pin 3 of PORTA
-    _delay_ms(500); 
-    PORTA &= ~(1<<PA2); // this line is equivalent to PORTA = PORTA & (0b11110111)  which writes a HIGH to pin 3 of PORTA
+    if (new_message_received_flag)
+    {
+      fc=dataByte1, rc=dataByte2;
+
+      lm = fc + rc - 253;
+      rm = fc - rc;
+
+      OCR3A = (int32_t)abs(lm) * 10000 / 126;
+      OCR3B = (int32_t)abs(rm) * 10000 / 126;
+
+      if (lm>=0)
+      {
+        // left motor goes forward
+        PORTA |= (1<<PA0);
+        PORTA &= ~(1<<PA1);
+      }
+      else
+      {
+        // left motor goes backward
+        PORTA &= ~(1<<PA0);
+        PORTA |= (1<<PA1);
+      }
+
+      if (rm>=0)
+      {
+        // right motor goes forward
+        PORTA |= (1<<PA2);
+        PORTA &= ~(1<<PA3);
+      }
+      else
+      {
+        // right motor goes backward
+        PORTA &= ~(1<<PA2);
+        PORTA |= (1<<PA3);
+      }
+
+      new_message_received_flag = false;
+    }
   }
   return(1);
 }//end main 
+
+ISR(USART2_RX_vect)
+{
+  static uint8_t recvByte1=0, recvByte2=0;		// data bytes received
+	static uint8_t serial_fsm_state=0;									// used in the serial receive ISR
+	uint8_t	serial_byte_in = UDR2; //move serial byte into variable
+	
+	switch(serial_fsm_state) //switch by the current state
+	{
+		case 0:
+		//do nothing, if check after switch case will find start byte and set serial_fsm_state to 1
+		break;
+		case 1: //waiting for first parameter
+		recvByte1 = serial_byte_in;
+		serial_fsm_state++;
+		break;
+		case 2: //waiting for second parameter
+		recvByte2 = serial_byte_in;
+		serial_fsm_state++;
+		break;
+		case 3: //waiting for stop byte
+		if(serial_byte_in == 0xFE) //stop byte
+		{
+			// now that the stop byte has been received, set a flag so that the
+			// main loop can execute the results of the message
+			dataByte1 = recvByte1;
+			dataByte2 = recvByte2;
+			
+			new_message_received_flag=true;
+		}
+		// if the stop byte is not received, there is an error, so no commands are implemented
+		serial_fsm_state = 0; //do nothing next time except check for start byte (below)
+		break;
+	}
+	if(serial_byte_in == 0xFF) //if start byte is received, we go back to expecting the first data byte
+	{
+		serial_fsm_state=1;
+	}
+}
